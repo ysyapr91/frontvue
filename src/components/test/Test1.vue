2ae83072f1bfa561903179bd6a10c78432824ea3
ysyapr91 <ysyapr91@gmail.com>
2021-10-07 18:11:30 +0900

    .

--- src/components/test/Test1.vue
+++ src/components/test/Test1.vue
@@ -1,5 +1,37 @@
 <template>
     <div id="test1">
-        <h3>test1</h3>
+        <button type="button" @click="localApi">Call</button>
+        {{apiRes}}
     </div>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'test1',
+  data () {
+    return {
+      apiRes: ''
+    }
+  },
+  computed: {
+    hasResult: function () {
+      return this.posts.length > 0
+    }
+  },
+  methods: {
+    localApi () {
+      const url = 'http://localhost:8081/'
+      axios
+        .get(url)
+        .then(function (res) {
+          console.log(res.data)
+        })
+        .catch(function (err) {
+          console.log(err)
+        })
+    }
+  }
+}
+</script>
